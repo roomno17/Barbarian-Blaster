119355ac4c9beb40eff376ef5f9c915c4de7f55c
roomno17 <2261021@flourbluffschools.org>
2026-01-21 13:08:03 -0600

    added turret and turretmanager

---
+++ ray_picker_camera.gd
@@ -0,0 +1,22 @@
+extends Camera3D
+@export var gridmap : GridMap
+@onready var ray_cast_3d: RayCast3D = $RayCast3D
+
+func _process(delta: float) -> void:
+	var mouse_position : Vector2 = get_viewport().get_mouse_position()
+	ray_cast_3d.target_position = project_local_ray_normal(mouse_position)*100
+	ray_cast_3d.force_raycast_update()
+	
+	if ray_cast_3d.is_colliding():
+		var collider = ray_cast_3d.get_collider()
+		if collider is GridMap:
+				var collision_point = ray_cast_3d.get_collision_point()
+				var cell = gridmap.local_to_map(collision_point)
+				if gridmap.get_cell_item(cell) ==0:
+					Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
+					if Input.is_action_just_pressed("click"):
+						gridmap.set_cell_item(cell,1)
+				else:
+					Input.set_default_cursor_shape(Input.CURSOR_ARROW)
+	else:
+					Input.set_default_cursor_shape(Input.CURSOR_ARROW)
