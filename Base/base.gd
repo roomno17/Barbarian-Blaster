40b8ceb8f39b38a144e296ea945129faab323c49
roomno17 <2261021@flourbluffschools.org>
2026-01-21 12:19:38 -0600

    working health label

--- Base/base.gd
+++ Base/base.gd
@@ -7,6 +7,9 @@ var health : int :
 	set(new_health):
 		health = new_health
 		label_3d.text = str(health) + "/" + str(max_health)
+		var red : Color = Color.RED
+		var white : Color = Color.WHITE
+		label_3d.modulate = red.lerp(white, float(health)/float(max_health))
 		if health == 0:
 			get_tree().reload_current_scene()
 @onready var label_3d: Label3D = $Label3D
