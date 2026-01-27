e5fa31937e1c9d8851e1cd792ed1680f51231110
roomno17 <2261021@flourbluffschools.org>
2026-01-27 12:40:09 -0600

    made level get harder

--- path_3d.gd
+++ path_3d.gd
@@ -1,8 +1,14 @@
 extends Path3D
 @export var enemy_scene: PackedScene
+@export var difficulty_manager : Node
+@onready var timer: Timer = $Timer
+
 func spawn_enemy():
 	var enemyinst = enemy_scene.instantiate()
 	add_child(enemyinst)
+	timer.wait_time = difficulty_manager.get_spawn_time()
+	print(timer.wait_time)
+	print(difficulty_manager.get_spawn_time())
 
 	
 
--- projectile.gd
+++ projectile.gd
@@ -5,14 +5,12 @@ var direction = Vector3.FORWARD
 func _physics_process(delta: float) -> void:
 	direction.y=0
 	position+=direction*speed*delta
-	print(position)
 
 func _on_timer_timeout() -> void:
 	queue_free()
 
 
 func _on_area_entered(area: Area3D) -> void:
-	print(area)
 	if area.is_in_group("enemy_area"):
 		area.get_parent().health-=25
 		queue_free()
