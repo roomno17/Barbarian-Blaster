3345c416f62034ec860012c9db4f2a0cca582068
roomno17 <2261021@flourbluffschools.org>
2026-01-28 13:08:05 -0600

    changed the victory screen

--- path_3d.gd
+++ path_3d.gd
@@ -1,17 +1,27 @@
 extends Path3D
-@export var enemy_scene: PackedScene
-@export var difficulty_manager : Node
 @onready var timer: Timer = $Timer
+@onready var victory_layer: CanvasLayer = $"../CanvasLayer"
 
-func spawn_enemy():
-	var enemyinst = enemy_scene.instantiate()
-	add_child(enemyinst)
-	enemyinst.max_health = difficulty_manager.get_enemy_health()
+@export var enemy_scene : PackedScene
+@export var difficulty_manager : Node
+func spawn_enemy() -> void:
+	var new_enemy = enemy_scene.instantiate()
+	new_enemy.max_health = difficulty_manager.get_enemy_health()
+	add_child(new_enemy)
+	new_enemy.tree_exited.connect(enemy_defeated)
 	timer.wait_time = difficulty_manager.get_spawn_time()
-	print(timer.wait_time)
-	print(difficulty_manager.get_spawn_time())
 
-	
+func enemy_defeated() -> void:
+	if timer.is_stopped():
+		for child in get_children():
+			if child is Enemy:
+				return
+		victory_layer.visible = true
+
 
 func _on_timer_timeout() -> void:
 	spawn_enemy()
+
+
+func _on_difficulty_manager_stop_spawning_enemies() -> void:
+	timer.stop()
